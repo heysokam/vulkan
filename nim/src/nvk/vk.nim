6b222ebfac11710854434082c35ede88a5b40a12
sOkam <itsfatum@gmail.com>
2023-09-15 09:37:28 +0200

    new: Hello Window

--- nim/src/nvk/vk.nim
+++ nim/src/nvk/vk.nim
@@ -1,10 +1,4 @@
 #:___________________________________________________
 #  nvk  |  Copyright (C) Ivan Mar (sOkam!)  |  MIT  :
 #:___________________________________________________
-
-when isMainModule:
-  import nsys
-  echo"hello n*vk"
-  var sys = nsys.init(960,540)
-  while not sys.close(): sys.update()
-  sys.term()
+import pkg/vulkan
