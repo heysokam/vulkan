5bb90c9235fcb191ea410b46672097b46b3ea98c
sOkam <7308253+heysokam@users.noreply.github.com>
2024-04-10 18:31:04 +0200

    wip: Some progress on instance initialization

--- zig/src/lib/vulkan.zig
+++ zig/src/lib/vulkan.zig
@@ -1,7 +1,43 @@
 //:__________________________________________________________________
 //  zvk  |  Copyright (C) Ivan Mar (sOkam!)  |  GNU GPLv3 or later  :
 //:__________________________________________________________________
+// @deps Vulkan
 const C = @cImport(@cInclude("vulkan/vulkan.h"));
-pub const Instance        = C.VkInstance;
-pub const instanceCreate  = C.vkCreateInstance;
-pub const instanceDestroy = C.vkDestroyInstance;
+
+//______________________________________
+// @section General Tools
+//____________________________
+pub const version   = struct {
+  pub const api     = struct {
+    pub const v1_0  = C.VK_API_VERSION_1_0;
+  };
+  pub const Default = api.v1_0;
+  pub const new     = C.VK_MAKE_VERSION;
+};
+
+//______________________________________
+// @section Application
+//____________________________
+pub const app = struct {
+  pub const Cfg = C.VkApplicationInfo;
+  pub fn defaults() app.Cfg {
+    return app.Cfg{
+      .sType              = C.VK_STRUCTURE_TYPE_APPLICATION_INFO,
+      .pNext              = null,
+      .pApplicationName   = "zvk.Application",
+      .applicationVersion = version.new(1, 0, 0),
+      .pEngineName        = "zvk.Engine",
+      .engineVersion      = version.new(1, 0, 0),
+      .apiVersion         = version.api.v1_0,
+      }; // << C.VkApplicationInfo{ ... }
+  }
+};
+
+//______________________________________
+// @section Instance
+//____________________________
+pub const Instance = C.VkInstance;
+pub const instance = struct {
+  pub const create  = C.vkCreateInstance;
+  pub const destroy = C.vkDestroyInstance;
+};
--- zig/src/zvk/core.zig
+++ zig/src/zvk/core.zig
@@ -8,4 +8,19 @@ pub const Vulkan = struct {
   pub fn update(m :*Vulkan) void {_=m;}
   pub fn term  (m :*Vulkan) void {_=m;}
 };
-pub fn init() Vulkan { return Vulkan{.instance=null}; }
+
+pub fn init() Vulkan {
+  var result = Vulkan{.instance=null};
+  const instanceCfg = vk.app.defaults();
+  vk.instance.create(
+    vk.app.Cfg{
+      .pApplicationName   = "Hello z*vk Triangle",
+      .applicationVersion = vk.version.new(1, 0, 0),
+      .pEngineName        = "Hello z*vk Triangle Engine",
+      .engineVersion      = vk.version.new(1, 0, 0),
+      },
+    null,
+    &result.instance,
+  ); // << vk.instance_create(InstanceCfg{ ... })
+  return result;
+}
