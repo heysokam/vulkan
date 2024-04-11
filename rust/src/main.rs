aaf59420814239d1d38e4c080a7cfc956ce8e616
sOkam <7308253+heysokam@users.noreply.github.com>
2024-04-11 03:50:40 +0200

    rust.new: Window initialization

---
+++ rust/src/main.rs
@@ -0,0 +1,59 @@
+#![allow(
+  dead_code,
+  clippy::too_many_arguments,
+  clippy::unnecessary_wraps
+)]
+
+use anyhow::Result;
+use winit::dpi::LogicalSize;
+use winit::event::{Event, WindowEvent};
+use winit::event_loop::EventLoop;
+use winit::window::{Window, WindowBuilder};
+
+fn main() -> Result<()> {
+  pretty_env_logger::init();
+
+  // Window
+  let event_loop = EventLoop::new()?;
+  let window = WindowBuilder::new()
+    .with_title("Vulkan Tutorial (Rust)")
+    .with_inner_size(LogicalSize::new(1024, 768))
+    .build(&event_loop)?;
+
+  // App
+  let mut app = unsafe { App::create(&window)? };
+  event_loop.run(move |event, elwt| { match event {
+    // Request a redraw when all events were processed.
+    Event::AboutToWait => window.request_redraw(),
+    Event::WindowEvent { event, .. } => match event {
+      // Render a frame if our Vulkan app is not being destroyed.
+      WindowEvent::RedrawRequested if !elwt.exiting() => unsafe { app.render(&window) }.unwrap(),
+      // Destroy our Vulkan app.
+      WindowEvent::CloseRequested => {
+        elwt.exit();
+        unsafe { app.destroy(); }
+      }
+      _ => {}
+    }
+    _ => {}
+  }})?;
+  Ok(())
+}
+
+/// Our Vulkan app.
+#[derive(Clone, Debug)]
+struct App {}
+
+impl App {
+  /// Creates our Vulkan app.
+  unsafe fn create(window: &Window) -> Result<Self> { Ok(Self {}) }
+  /// Renders a frame for our Vulkan app.
+  unsafe fn render(&mut self, window: &Window) -> Result<()> { Ok(()) }
+  /// Destroys our Vulkan app.
+  unsafe fn destroy(&mut self) {}
+}
+
+/// The Vulkan handles and associated properties used by our Vulkan app.
+#[derive(Clone, Debug, Default)]
+struct AppData {}
+
