f06e12c5d30f545ee3c13e63e0161d1a6d4904e2
sOkam <7308253+heysokam@users.noreply.github.com>
2024-10-02 06:33:35 +0200

    fmt.zig: Entry file sketch for `zgpu`

---
+++ zig/zgpu.zig
@@ -0,0 +1,9 @@
+//:__________________________________________________________________
+//  zgpu  |  Copyright (C) Ivan Mar (sOkam!)  |  GNU GPLv3 or later  :
+//:__________________________________________________________________
+// @deps zdk
+const zstd = @import("./zstd.zig");
+const echo = zstd.echo;
+const zsys = @import("./zsys.zig");
+const zvk  = @import("./zvk.zig");
+
