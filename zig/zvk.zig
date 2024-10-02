e226ae2d2afa3a2c81deb9bc756bd214ab41e91b
sOkam <7308253+heysokam@users.noreply.github.com>
2024-10-02 06:32:34 +0200

    fmt.zig: Folder structure sketch for `zvk`

--- zig/zvk.zig
+++ zig/zvk.zig
@@ -1,20 +1,8 @@
 //:__________________________________________________________________
 //  zvk  |  Copyright (C) Ivan Mar (sOkam!)  |  GNU GPLv3 or later  :
 //:__________________________________________________________________
-// z*dk dependencies
-const zstd = @import("./zstd.zig");
-const zsys = @import("./zsys/core.zig");
-const zvk  = @import("./zvk/core.zig");
-
-pub fn main() !void {
-  try zstd.echo("Hello zig.GLFW");
-  var sys = zsys.init(960,540,"Hello z*Sys");
-  var gpu = zvk.init();
-  while (!sys.close()) {
-    sys.update();
-    gpu.update();
-    }
-  gpu.term();
-  sys.term();
-}
-
+//! @fileoverview Cable connector to the zvk modules
+//_____________________________________________________|
+pub usingnamespace @import("./zvk/types.zig");
+pub usingnamespace @import("./zvk/elements.zig");
+pub usingnamespace @import("./zvk/tools.zig");
---
+++ zig/zvk/tools.zig
@@ -0,0 +1,7 @@
+//:__________________________________________________________________
+//  zvk  |  Copyright (C) Ivan Mar (sOkam!)  |  GNU GPLv3 or later  :
+//:__________________________________________________________________
+// @deps vulkan
+const vk = @import("../lib/vulkan.zig");
+// @deps zdk
+const zstd = @import("../zstd.zig");
