7a126b5496a30b2a7413d6cf59942fd6b0a5b747
sOkam <7308253+heysokam@users.noreply.github.com>
2024-10-03 12:20:16 +0200

    doc.zig: Reference notes for zvk

--- zig/zvk.zig
+++ zig/zvk.zig
@@ -6,3 +6,13 @@
 pub usingnamespace @import("./zvk/types.zig");
 pub usingnamespace @import("./zvk/elements.zig");
 pub usingnamespace @import("./zvk/tools.zig");
+
+
+//______________________________________
+// TODO: External zvk_thing_fn API for C
+const C = struct {
+  export fn zvk_tmp_add  (a :c_int, b :c_int) c_int { return a + b; }
+  export fn zvk_tmp_add2 (a :  u32, b :  u32)   u32 { return a + b; }
+}; comptime { _ = &C; } // Force exports to be analyzed
+//______________________________________
+
