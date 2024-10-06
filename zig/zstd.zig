4d59bda982c8c8bc2e94382f26559425f7b479bc
sOkam <7308253+heysokam@users.noreply.github.com>
2024-10-06 11:44:40 +0200

    new.zig: zstd: Compile mode checks

--- zig/zstd.zig
+++ zig/zstd.zig
@@ -2,14 +2,21 @@
 //  zstd  |  Copyright (C) Ivan Mar (sOkam!)  |  GNU GPLv3 or later  :
 //:___________________________________________________________________
 // @deps std
-const std  = @import("std");
-
+const std     = @import("std");
+const builtin = @import("builtin");
 //______________________________________
 // @section Operating System Aliases
 //____________________________
-pub const linux = @import("builtin").os.tag == .linux;
-pub const win   = @import("builtin").os.tag == .windows;
-pub const macos = @import("builtin").os.tag.isDarwin();
+pub const linux = builtin.os.tag == .linux;
+pub const win   = builtin.os.tag == .windows;
+pub const macos = builtin.os.tag.isDarwin();
+
+//______________________________________
+// @section Compile Mode Aliases
+//____________________________
+pub const debug = builtin.mode == std.builtin.OptimizeMode.Debug;
+pub const safe  = std.debug.runtime_safety;
+
 
 //______________________________________
 // @section Type Aliases
