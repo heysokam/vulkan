2085fbeab1bd5d06a0826ddf348dd73330cd01fd
sOkam <7308253+heysokam@users.noreply.github.com>
2024-10-19 01:11:46 +0200

    rew.zig: Hello World

---
+++ zig/entry.zig
@@ -0,0 +1,11 @@
+//:_____________________________________________________________________________________
+//  Vulkan: All-the-Things  |  Copyright (C) Ivan Mar (sOkam!)  |  GNU GPLv3 or later  :
+//:_____________________________________________________________________________________
+// @deps std
+const std = @import("std");
+
+pub fn main () !u8 {
+  std.debug.print("Hello zvk Entry\n", .{});
+  return 0;
+}
+
