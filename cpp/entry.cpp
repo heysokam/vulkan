04e71214f78a3b70d1548e3c1fcf2a4eb2b27570
sOkam <7308253+heysokam@users.noreply.github.com>
2024-10-19 01:11:40 +0200

    rew.cpp: Hello World

---
+++ cpp/entry.cpp
@@ -0,0 +1,12 @@
+//:__________________________________________________________________
+//  vk+  |  Copyright (C) Ivan Mar (sOkam!)  |  GNU GPLv3 or later  :
+//:__________________________________________________________________
+// @deps std
+#include <iostream>
+
+int main (int const argc, char const* argv[argc]) {
+  std::cout << "Hello vk+ Entry" << std::endl;
+  return 0;
+} //:: main
+
+
