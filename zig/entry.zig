4ece531058a0852c7b5bbca6e8dcd88cece2c84e
sOkam <7308253+heysokam@users.noreply.github.com>
2024-10-19 10:43:28 +0200

    rew.zig: Hello Window with zdk

--- zig/entry.zig
+++ zig/entry.zig
@@ -3,9 +3,44 @@
 //:_____________________________________________________________________________________
 // @deps std
 const std = @import("std");
+// @deps zdk
+const zstd = @import("./zstd.zig");
+const zsys = @import("./zsys.zig");
+const zvk  = @import("./zvk.zig").zvk;
 
-pub fn main () !u8 {
-  std.debug.print("Hello zvk Entry\n", .{});
-  return 0;
-}
+
+//:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+//:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+//:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+const shd = struct {
+  const vert align(@alignOf(u32)) = @embedFile("shd_vert").*;
+  const frag align(@alignOf(u32)) = @embedFile("shd_frag").*;
+};
+const appName = "Zig | Vulkan-All-the-Things | Triangle";
+var size      = zvk.Size{.width= 960, .height= 540};
+
+//______________________________________
+// @section Entry Point
+//____________________________
+pub fn main () !void {
+  var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+  defer arena.deinit();
+  // const A = zgpu.Allocator.create(arena.allocator());
+
+  try zstd.echo("Hello zvk Entry");
+  try zstd.echo(appName);
+  var sys = try zsys.init(size.width, size.height, appName);
+  // var gpu = try zgpu.init(.{
+  //   .appName    = appName,
+  //   .appVers    = zgpu.version.new(0, 0, 1),
+  //   .engineName = "z*gpu | Triangle Engine",
+  //   .engineVers = zgpu.version.new(0, 0, 1),
+  //   }, A);
+  while (!sys.close()) {
+    sys.update();
+    // gpu.update();
+    }
+  // gpu.term();
+  sys.term();
+} //:: main
 
