ef8acb20612136c7ecd6c0442a2e2e2d6613bc96
sOkam <7308253+heysokam@users.noreply.github.com>
2025-05-31 04:30:12 +0200

    chg.cpp: Alias stdint types from `std::` instead of directly

--- cpp/cstd.hpp
+++ cpp/cstd.hpp
@@ -11,21 +11,21 @@
 #include <cstdint>
 #include <cstddef>
 #include <cstring>
-using byte = uint8_t;
-using u8   = uint8_t;
-using u16  = uint16_t;
-using u32  = uint32_t;
-using u64  = uint64_t;
-using uP   = uintptr_t;
+using byte = std::uint8_t;
+using u8   = std::uint8_t;
+using u16  = std::uint16_t;
+using u32  = std::uint32_t;
+using u64  = std::uint64_t;
+using uP   = std::uintptr_t;
 using Sz   = std::size_t;
-using i8   = int8_t;
-using i16  = int16_t;
-using i32  = int32_t;
-using i64  = int64_t;
-using iP   = intptr_t;
+using i8   = std::int8_t;
+using i16  = std::int16_t;
+using i32  = std::int32_t;
+using i64  = std::int64_t;
+using iP   = std::intptr_t;
 using f32  = float;
 using f64  = double;
-using TODO = uint8_t;
+using TODO = std::uint8_t;
 #include <iostream>
 using str  = std::string;
 using cstr = char const*;
