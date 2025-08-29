a247e0768a1d39c50ec39b677453420c8c47c4e5
sOkam <7308253+heysokam@users.noreply.github.com>
2025-08-29 08:53:51 +0200

    rmv.nim: Delete Nim implementation. Will not write

--- nim/nvk/allocator.nim
+++
@@ -1,9 +0,0 @@
-#:__________________________________________________________________
-#  ndk  |  Copyright (C) Ivan Mar (sOkam!)  |  GNU GPLv3 or later  :
-#:__________________________________________________________________
-# @deps vulkan
-from ../nvulkan as vk import nil
-
-type Allocator * = object
-  vk  *:ptr vk.Allocator=  nil
-
